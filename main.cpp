167d3a39f7f0edb8b4e971cbd745a5db2ec97bc5
persiKey <parhomchuk308@gmail.com>
2023-01-04 18:46:05 +0200

    made report

--- main.cpp
+++ main.cpp
@@ -3,23 +3,29 @@
 #include <time.h>
 #include <random>
 #include "main.h"
+#include "fstream"
 
 using std::cin;
 using std::cout;
 
 int main()
 {
-	//srand(time(NULL));
+	srand(time(NULL));
 
 	Population p1;
-
-	for (size_t i = 0; i < 1000; i++)
+	std::ofstream file("data.csv");
+	file << "Iteration , Max F Value\n";
+	for (size_t i = 0; i <= 1000; i++)
 	{
 		if (!(i % 20))
+		{
+			file << i << " , " << p1.GetMaxWorth() << '\n';
 			cout << p1.GetMaxWorth() << '\n';
+		}
 		p1.Iterate();
 	}
 	p1.PrintMax();
+	file.close();
 	char key;
 	int num;
 	do
